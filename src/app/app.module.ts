4cd1792f9c7489044b7670a1f276af3fc7451533
Maxime CHEVALLIER-PICHON <maxime.chevallier_pichon@yahoo.fr>
2019-03-19 20:52:26 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DetailComponent } from './detail/detail.component';
+import { ListeComponent } from './liste/liste.component';
+import { AjoutComponent } from './ajout/ajout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DetailComponent,
+    ListeComponent,
+    AjoutComponent,
   ],
   imports: [
     BrowserModule,
